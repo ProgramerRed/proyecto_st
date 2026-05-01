c3936e7ccba372190ae8b54783ceefe44d9fa699
ProgramerRed <danielarturo2112@gmail.com>
2026-05-01 17:07:20 -0400

    Paso 7: Ultimo Commit

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
 
-echo "Proyecto Funcionando";
-
-
+use App\Controllers\ProyectoController;
+$controller = new ProyectoController();
+$action = isset($_GET['action']) ? $_GET['action'] : 'index';
+switch ($action) {
+    case 'index':
+        $controller->index();
+        break;
+    case 'crear':
+        $controller->crear();
+        break;
+    case 'guardar':
+        $controller->guardar();
+        break;
+    case 'eliminar':
+        $controller->eliminar();
+        break;
+    default:
+        $controller->index();
+}
